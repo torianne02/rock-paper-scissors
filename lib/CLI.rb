d906953ffc59de9b4ddd6b5210a47611c4373cb6
torianne02 <victoria.fluharty@gmail.com>
2019-07-01 11:46:58 -0700

    adds break statements to while loops adds another while loop in game_play adds logic to check if input is valid

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -9,8 +9,10 @@ class RockPaperScissors::CLI
 
       if input == "yes" || input == "y"
         play_game
+        break;
       elsif input == "no" || input == "n"
         puts "Thank you for visiting!"
+        break;
       else
         puts "Invalid input. Please try again."
       end
@@ -20,8 +22,17 @@ class RockPaperScissors::CLI
   def play_game
     puts "Please choose your move: rock, paper, or scissors?"
 
-    input = gets.chomp.downcase
-    game = RockPaperScissors::Game.new(input)
+    while true
+      input = gets.chomp.downcase
+      options = ["rock", "paper", "scissors"]
+      
+      if options.any? {|o| input.include?(o)}
+        game = RockPaperScissors::Game.new(input)
+        break;
+      else
+        puts "Invalid input. Please try again."
+      end
+    end
 
     puts game.winner_is
     play_again?
@@ -35,8 +46,10 @@ class RockPaperScissors::CLI
 
       if input == "yes" || input == "y"
         play_game
+        break;
       elsif input == "no" || input == "n"
         puts "Thank you for playing."
+        break;
       else
         puts "Invalid input. Please try again."
       end
