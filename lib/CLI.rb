bcd3a4b74aa334484d2dbb5252b5bc030bc74749
torianne02 <victoria.fluharty@gmail.com>
2019-07-01 11:34:46 -0700

    adds while loops to CLI.rb while loops needed so command line is not exited when invalid input

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -4,13 +4,16 @@ class RockPaperScissors::CLI
   def call
     puts "Would you like to play a game of rock, paper, scissors?"
 
-    input = gets.chomp.downcase
-    if input == "yes" || input == "y"
-      play_game
-    elsif input == "no" || input == "n"
-      puts "Thank you for visiting!"
-    else
-      puts "Invalid input. Please try again."
+    while true
+      input = gets.chomp.downcase
+
+      if input == "yes" || input == "y"
+        play_game
+      elsif input == "no" || input == "n"
+        puts "Thank you for visiting!"
+      else
+        puts "Invalid input. Please try again."
+      end
     end
   end
 
@@ -20,23 +23,23 @@ class RockPaperScissors::CLI
     input = gets.chomp.downcase
     game = RockPaperScissors::Game.new(input)
 
-    puts "Computer played #{game.comp_move}"
-
     puts game.winner_is
-
     play_again?
   end
 
   def play_again?
     puts "Would you like to play again?"
 
-    input = gets.chomp.downcase
-    if input == "yes" || input == "y"
-      play_game
-    elsif input == "no" || input == "n"
-      puts "Thank you for playing."
-    else
-      puts "Invalid input. Please try again."
+    while true
+      input = gets.chomp.downcase
+
+      if input == "yes" || input == "y"
+        play_game
+      elsif input == "no" || input == "n"
+        puts "Thank you for playing."
+      else
+        puts "Invalid input. Please try again."
+      end
     end
   end
 end
